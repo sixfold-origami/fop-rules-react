939b425ff089d8774bc180d6b2225c130b8e18b1
Rose Peck <olaf223224@gmail.com>
2020-07-02 11:03:19 -0500

    Updates

---
+++ precache-manifest.879837796b4c6e34d6eb39bd33811247.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36c0674625aa4e473112497c22401778",
+    "url": "/fop-rules/index.html"
+  },
+  {
+    "revision": "da051f01be5721eb48d6",
+    "url": "/fop-rules/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6685c29498b45fa4bd35",
+    "url": "/fop-rules/static/js/2.5288eada.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fop-rules/static/js/2.5288eada.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da051f01be5721eb48d6",
+    "url": "/fop-rules/static/js/main.1b886c54.chunk.js"
+  },
+  {
+    "revision": "b6686fe61ede5ac5877a",
+    "url": "/fop-rules/static/js/runtime-main.d45673d6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fop-rules/static/media/logo.5d5d9eef.svg"
+  }
+]);
